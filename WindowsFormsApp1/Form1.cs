90606446c2d239c41ccb57d01874a37635c57b2b
Ivan Tkachuk <ivtkac@gmail.com>
2021-12-21 19:30:54 +0200

    Show Table

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,17 +7,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private readonly string server = "localhost";
+        private readonly string database = "test";
+        private readonly string uid = "root";
+        private readonly string password = "";
+        private string table = "";
+        private MySqlConnection connection;
+        private MySqlDataAdapter mySqlDataAdapter;
+
         public Form1()
         {
             InitializeComponent();
+            connection = new MySqlConnection($"server={server};database={database};uid={uid};password={password}");
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.OpenConnection() == true)
+            {
+                mySqlDataAdapter = new MySqlDataAdapter($"select * from {table}", connection);
+                DataSet DS = new DataSet();
+                mySqlDataAdapter.Fill(DS);
+                dataGridView1.DataSource = DS.Tables[0];
+
+                this.CloseConnection();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedItem = comboBox1.GetItemText(comboBox1.SelectedItem);
+            table = selectedItem;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataTable changes = ((DataTable)dataGridView1.DataSource).GetChanges();
+
+            if (changes != null)
+            {
+                MySqlCommandBuilder mcb = new MySqlCommandBuilder(mySqlDataAdapter);
+                mySqlDataAdapter.UpdateCommand = mcb.GetUpdateCommand();
+                mySqlDataAdapter.Update(changes);
+                ((DataTable)dataGridView1.DataSource).AcceptChanges();
+            }
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            } catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server. Contact administrator");
+                        break;
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                    default:
+                        MessageBox.Show(ex.Message);
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            } catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
